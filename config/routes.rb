32ca267e93152999379171a7d78f1fb0a411116b
Hugo Lennep <hugovanlennep@gmail.com>
2023-09-22 14:47:58 +0200

    Made some final changes to user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, only: :show
   root to: "pages#home"
   get "profile", to: "pages#profile"
   get "calendar", to: "pages#calendar"
