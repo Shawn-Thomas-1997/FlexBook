6a0572d950783f73a9e60ba13db72c76dc788024
Hugo Lennep <hugovanlennep@gmail.com>
2023-09-22 13:24:38 +0200

    added a bunch of new things

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookings
-  has_many :listings
-  has_many :certifications
+  has_many :bookings, dependent: :destroy
+  has_many :listings, dependent: :destroy
+  has_many :certifications, dependent: :destroy
   has_one_attached :photo
 end
